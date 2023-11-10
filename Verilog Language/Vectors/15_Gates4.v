d5c5459f2b55368efdbc5ac2b0df5fd90d789459
Shruti Tajne <150401115+Shruti-Tajne@users.noreply.github.com>
2023-11-10 03:05:11 -0600

    Create 15_Gates4.v

---
+++ Verilog Language/Vectors/15_Gates4.v
@@ -0,0 +1,13 @@
+// Question 15. Four-input gates/ Gates4 - https://hdlbits.01xz.net/wiki/Gates4
+
+module top_module( 
+    input [3:0] in,
+    output out_and,
+    output out_or,
+    output out_xor
+);
+    assign out_and = in[3] & in[2] & in[1] & in[0];
+    assign out_or = in[3] | in[2] | in[1] | in[0];
+    assign out_xor = in[3] ^ in[2] ^ in[1] ^ in[0];
+
+endmodule
