79bc2593c6c89be58743e40bde24d693946d8fc7
Shruti Tajne <150401115+Shruti-Tajne@users.noreply.github.com>
2023-11-10 02:27:13 -0600

    Create 11. Vector0.v

---
+++ Verilog Language/Vectors/11. Vector0.v
@@ -0,0 +1,18 @@
+// Question 11. Vector0 - https://hdlbits.01xz.net/wiki/Vector0
+
+module top_module(
+	input [2:0] vec, 
+	output [2:0] outv,
+	output o2,
+	output o1,
+	output o0
+);
+	
+	assign outv = vec;
+
+	// This is ok too: assign {o2, o1, o0} = vec;
+	assign o0 = vec[0];
+	assign o1 = vec[1];
+	assign o2 = vec[2];
+	
+endmodule
