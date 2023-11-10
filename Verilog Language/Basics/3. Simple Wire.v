efb24563f4f2aaa5351427570e4bd78da39a54b6
Shruti Tajne <150401115+Shruti-Tajne@users.noreply.github.com>
2023-11-10 02:24:41 -0600

    Create 3. Simple Wire.v

---
+++ Verilog Language/Basics/3. Simple Wire.v
@@ -0,0 +1,9 @@
+//Question 3.Wire - https://hdlbits.01xz.net/wiki/Wire
+
+module top_module( input in, output out );
+	
+	assign out = in;
+	// Note that wires are directional, so "assign in = out" is not equivalent.
+	
+endmodule
+
