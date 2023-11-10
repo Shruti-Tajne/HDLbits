1b41c846b6e64c03c21fb6efe5a774b9e811d208
Shruti Tajne <150401115+Shruti-Tajne@users.noreply.github.com>
2023-11-10 03:11:54 -0600

    Create 18_Vector4.v

---
+++ Verilog Language/Vectors/18_Vector4.v
@@ -0,0 +1,13 @@
+// Question 18. Replication Operator/ Vector4 - https://hdlbits.01xz.net/wiki/Vector4
+
+module top_module (
+	input [7:0] in,
+	output [31:0] out
+);
+
+	// Concatenate two things together:
+	// 1: {in[7]} repeated 24 times (24 bits)
+	// 2: in[7:0] (8 bits)
+	assign out = { {24{in[7]}}, in };
+	
+endmodule
