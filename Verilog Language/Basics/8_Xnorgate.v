1aaa49b617be4e53c31e3c9e6f38e30c89bfd08b
Shruti Tajne <150401115+Shruti-Tajne@users.noreply.github.com>
2023-11-10 02:52:43 -0600

    Create 8_Xnorgate.v

---
+++ Verilog Language/Basics/8_Xnorgate.v
@@ -0,0 +1,10 @@
+// Question 8. Xnorgate - https://hdlbits.01xz.net/wiki/Xnorgate
+
+module top_module( 
+    input a, 
+    input b, 
+    output out );
+  
+    assign out = ~(a^b);
+  
+endmodule
