9ca9d2724cabccbf05fb8558c83331b6077b2aec
Shruti Tajne <150401115+Shruti-Tajne@users.noreply.github.com>
2023-11-10 02:28:23 -0600

    Update and rename 1. Getting Started to 1. Getting Started.v

--- Getting Started/1. Getting Started.v
+++ Getting Started/1. Getting Started.v
@@ -1,3 +1,5 @@
+// Question 1. Step One - https://hdlbits.01xz.net/wiki/Step_one
+
 module top_module( output one );
 
 //"one" is the name we gave to a "literal wire" and connected it to VDD so it will always remain high
